1f3931e835752e851dd46fe12a82c40230be2904
LOppitz <louis.oppitz04@htl-salzburg.ac.at>
2021-01-11 13:08:37 +0100

    errors fixed - timer & player are working r Please enter the commit message for your changes. Lines starting

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -10,13 +10,11 @@ public class PlayerController : MonoBehaviour
     public GameObject player;
 
     private float moveHorizontal;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveHorizontal = Input.GetAxis("Horizontal") * speed;
